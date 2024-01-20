b8153e96d9973c29ac2ebba00a089efea4b22372
Aditya Patel <115590216+aditya9522@users.noreply.github.com>
2024-01-21 01:03:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+window.onload =  function() {
+    setTimeout(() => {
+        document.querySelector('#loader').style.display = 'none';
+        document.querySelector('.main').style.display = 'block';
+    }, 600);
+
+    setTimeout(() => {
+        document.querySelector('.main').style.filter = 'blur(10px)';
+    }, 4500);
+
+    setTimeout(() => {
+        document.querySelector('.welcome-popup').style.display = 'block';
+    }, 5000);
+    
+};
+
+function closePopup() {
+    document.querySelector('.welcome-popup').style.display = 'none';
+    document.querySelector('.main').style.filter = 'none';
+}
 
 const drawMenu = document.querySelector(".draw-menu");
 const menuList = document.querySelector(".menu-list");
+const animElement = document.getElementsByClassName('rise-animation');
 let topbutton = document.querySelector('.gotop');
 
-// to hide or unhide the navbar
 drawMenu.addEventListener("click", function () {
     menuList.classList.toggle("active");
     this.classList.toggle('clicked');
 });
+
+for (let index = 0; index < animElement.length; index++) {
+    animElement[index].addEventListener('mouseover', () =>{
+        animElement[index].classList.add('animation');
+    });
+}
 
 // to get visibility to gotop button after scrolling 600px
 window.onscroll = function() {scrollFunction()};
@@ -26,4 +52,3 @@ function gototop() {
     document.body.scrollTop = 0;       // works in safari
     document.documentElement.scrollTop = 0;   // works in crome, firefox, opera, internet explorer
 }
-
